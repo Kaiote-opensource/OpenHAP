606a1225d510c9c3ddf56c08c84488cdb61d0d3d
Adams Okode <adamsokode@gmail.com>
2019-03-19 10:50:09 +0300

    div assignment update

--- IAQ_prototype/Hardware/firmware/www/js/status.js
+++ IAQ_prototype/Hardware/firmware/www/js/status.js
@@ -2,13 +2,13 @@ const SOCKET_URL = "ws://192.168.4.1:80";
 const LOG = true
 
 var sample = {
-  "HUM":	67,
-	"TEMP":	99,
-	"PM2.5":	8,
-	"TIME":	19,
-	"RTC_BATT":	10,
-	"MAIN BATT_BATT":	68,
-	"SD CARD":	"DISCONNECTED"
+  "HUM": 67,
+  "TEMP": 99,
+  "PM2.5": 8,
+  "TIME": 19,
+  "RTC_BATT": 10,
+  "MAIN BATT_BATT": 68,
+  "SD CARD": "DISCONNECTED"
 }
 
 
@@ -29,10 +29,20 @@ var socket = {
   },
   onMessage: function (evt) {
     helpers.log(evt)
+
+    document.getElementById("pm25-val").innerHTML = evt["PM2.5"] + "μg/m3"
+    document.getElementById("temp-val").innerHTML = evt.TEMP + "C"
+    document.getElementById("hum-val").innerHTML = evt.HUM + ""
   },
   onError: function (evt) {
     helpers.log(evt)
+  },
+  startMeasuring: function () {
+    websocketInst.send({
+      MEASUREMENT: true
+    })
   }
+
 
 }
 
