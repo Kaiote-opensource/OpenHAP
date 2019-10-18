a07934cc23de1f2382713f45553925b836d8672b
Alois Mbutura <aloismbutura@gmail.com>
2019-10-18 11:36:37 +0300

    Finalise particulate matter sensor src code

--- IAQ_prototype/Hardware/firmware/components/particulate_matter_sensors/uart_zh03.c
+++ IAQ_prototype/Hardware/firmware/components/particulate_matter_sensors/uart_zh03.c
@@ -10,7 +10,7 @@
  */
 /**********************************************************************************************************************************/
 #define RX_BUF_SIZE 256
-#define ZH03_UART_TIMEOUT 1500;
+#define ZH03_UART_TIMEOUT 1500
 
 /**********************************************************************************************************************************/
 /**
@@ -43,14 +43,14 @@
 #define ZH03_UPLOAD_START_BYTE_POS             0
 #define ZH03_UPLOAD_START_BYTE_2_POS           1
 #define ZH03_UPLOAD_FRAME_LENGTH_POS           3
-#define ZH03_UPLOAD_PM_1_HIGH_BYTE_POS         10
-#define ZH03_UPLOAD_PM_1_LOW_BYTE_POS          11
-#define ZH03_UPLOAD_PM_2_5_HIGH_BYTE_POS       12
-#define ZH03_UPLOAD_PM_2_5_LOW_BYTE_POS        13
-#define ZH03_UPLOAD_PM_10_HIGH_BYTE_POS        14
-#define ZH03_UPLOAD_PM_10_LOW_BYTE_POS         15
-#define ZH03_UPLOAD_CHECKSUM_HIGH_BYTE_POS     22
-#define ZH03_UPLOAD_CHECKSUM_LOW_BYTE_POS      23
+#define ZH03_UPLOAD_PM1_HIGH_BYTE_POS         10
+#define ZH03_UPLOAD_PM1_LOW_BYTE_POS          11
+#define ZH03_UPLOAD_PM2_5_HIGH_BYTE_POS       12
+#define ZH03_UPLOAD_PM2_5_LOW_BYTE_POS        13
+#define ZH03_UPLOAD_PM10_HIGH_BYTE_POS        14
+#define ZH03_UPLOAD_PM10_LOW_BYTE_POS         15
+#define ZH03_UPLOAD_CHECKSUM_HIGH_BYTE_POS    22
+#define ZH03_UPLOAD_CHECKSUM_LOW_BYTE_POS     23
 
 
 static const char* TAG = "PM_SENSOR_ZH03";
@@ -113,6 +113,12 @@ enum
     ZH03_MAX =255
 } zh03_command_t;
 
+enum
+{
+    ZH03_INITIATIVE_MEASUREMENT = 0,
+    ZH03_CMD_RESPONSE,
+    ZH03_MAX =255
+} zh03_data_t;
 
 esp_err_t zh03_init(ZH03* zh03_inst, uart_port_t uart_num, int txd_pin, int rxd_pin)
 {
@@ -124,9 +130,9 @@ esp_err_t zh03_init(ZH03* zh03_inst, uart_port_t uart_num, int txd_pin, int rxd_
     
     uart_config_t uart_config =
     {
-            /**
-            * 9600 baud 8N1
-            */
+        /**
+        * 9600 baud 8N1
+        */
         .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
@@ -150,6 +156,17 @@ esp_err_t zh03_init(ZH03* zh03_inst, uart_port_t uart_num, int txd_pin, int rxd_
         zh03_inst->uart_port = uart_num;
     }
     return ret;
+}
+
+/* Convert (u)int8 variable type to (u)int16 */
+static uint16_t conv_int_8_16(uint8_t msb, uint8_t lsb)
+{
+    ESP_LOGD(TAG, "ENTERED FUNCTION [%s]", __func__);
+    uint16_t combined = 0x0000;
+    combined = msb;
+    combined = combined << 8;
+    combined |= lsb;
+    return combined;
 }
 
 static esp_err_t zh03_uart_write(const ZH03* zh03_inst, const void* out_data, size_t out_size)
@@ -191,7 +208,7 @@ static esp_err_t zh03_uart_read(const ZH03* zh03_inst, uint8_t* in_data, int* in
     return ESP_OK;
 }
 
-static esp_err_t zh03_verify_checksum_8(uint8_t* data, int length)
+static esp_err_t zh03_verify_checksum(zh03_data_t data_type, uint8_t* data, int length)
 {
     ESP_LOGD(TAG, "ENTERED FUNCTION [%s]", __func__);
     if(data == NULL || length <= 0)
@@ -199,58 +216,54 @@ static esp_err_t zh03_verify_checksum_8(uint8_t* data, int length)
         return ESP_ERR_INVALID_ARG;
     }
 
-    uint8_t calculated_checksum= 0;
-    uint8_t frame_checksum = 0;
-
-    for(int j = 0; j < length; j++)
+    if(data_type == ZH03_CMD_RESPONSE)
     {
-        calculated_checksum += data[j];
-    }
+        uint8_t calculated_checksum= 0;
+        uint8_t frame_checksum = 0;
 
-    calculated_checksum = ~calculated_checksum;
-    calculated_checksum += 1;
-    ESP_LOGD(TAG, "Calculated checksum is 0x%02x", (int)calculated_checksum);
-    sent_checksum = data[i+(ZH03_CMD_FRAME_LENGTH-1)];
-    ESP_LOGD(TAG, "Frame checksum is 0x%02x", (int)frame_checksum);
-    if(checksum != frame_checksum)
+        for(int i = ZH03_CMD_ID_POS; i < length-sizeof(frame_checksum); i++)
+        {
+            calculated_checksum += data[j];
+        }
+
+        calculated_checksum = ~calculated_checksum;
+        calculated_checksum += 1;
+        ESP_LOGD(TAG, "Calculated checksum is 0x%02x", (int)calculated_checksum);
+        frame_checksum = data[ZH03_QA_CMD_CHECKSUM];
+        ESP_LOGD(TAG, "Frame checksum is 0x%02x", (int)frame_checksum);
+        if(calculated_checksum != frame_checksum)
+        {
+            ESP_LOGE(TAG, "Checksum verification failed!");
+            return ESP_FAIL;
+        }
+
+        ESP_LOGI(TAG, "Checksum verification ok!"); 
+        return ESP_OK;
+    }
+    else
     {
-        ESP_LOGE(TAG, "Checksum failure!");
-        return ESP_FAIL;
+        uint16_t calculated_checksum = 0;
+        uint16_t frame_checksum = 0;
+
+        for(int i = ZH03_UPLOAD_START_BYTE_POS; i < length-sizeof(frame_checksum); i++)
+        {
+            calculated_checksum += data[i];
+        }
+
+        calculated_checksum = calculated_checksum%0xffff;
+        ESP_LOGD(TAG, "Calculated checksum is 0x%04x", (int)calculated_checksum);
+        frame_checksum = conv_int_8_16(data[ZH03_CHECKSUM_HIGH_BYTE_POS], data[ZH03_CHECKSUM_LOW_BYTE_POS]);
+        ESP_LOGD(TAG, "Frame checksum is 0x%04x", (int)frame_checksum);  
+        if(calculated_checksum != frame_checksum)
+        {
+            ESP_LOGE(TAG, "Checksum verification failed!");
+            return ESP_FAIL;
+        }
+
+        ESP_LOGI(TAG, "Checksum verification ok!"); 
+        return ESP_OK; 
     }
-
-    ESP_LOGI(TAG, "Checksum success!"); 
-    return ESP_OK;   
-}
-
-static esp_err_t zh03_verify_checksum_16(uint8_t* data, int length)
-{
-    ESP_LOGD(TAG, "ENTERED FUNCTION [%s]", __func__);
-    if(data == NULL || length <= 0)
-    {
-        return ESP_ERR_INVALID_ARG;
-    }
-
-    uint8_t calculated_checksum= 0;
-    uint8_t frame_checksum = 0;
-
-    for(int j = 0; j < length; j++)
-    {
-        calculated_checksum += data[j];
-    }
-
-    calculated_checksum = ~calculated_checksum;
-    calculated_checksum += 1;
-    ESP_LOGD(TAG, "Calculated checksum is 0x%02x", (int)calculated_checksum);
-    sent_checksum = data[i+(ZH03_CMD_FRAME_LENGTH-1)];
-    ESP_LOGD(TAG, "Frame checksum is 0x%02x", (int)frame_checksum);
-    if(checksum != frame_checksum)
-    {
-        ESP_LOGE(TAG, "Checksum failure!");
-        return ESP_FAIL;
-    }
-
-    ESP_LOGI(TAG, "Checksum success!"); 
-    return ESP_OK;   
+       
 }
 
 static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh03_command_t command_type_sent, uint16_t* PM1, uint16_t* PM2_5, uint16_t* PM10)
@@ -276,12 +289,13 @@ static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh0
                     if(command_type_sent == ZH03_CMD_DORMANCY_STATE_CHANGE && frame_data[ZH03_CMD_ID_POS] == dormancy_state_change_response_id)
                     {
                         ESP_LOGD(TAG, "Successfully found response to dormancy state change command");
-                        esp_err_t ret = zh03_verify_checksum(frame_data[ZH03_CMD_ID_POS], ZH03_CMD_FRAME_LENGTH-1);
-                        if(ret != ESP_OK)
+                        if(zh03_verify_checksum(ZH03_CMD_RESPONSE, frame_data, ZH03_CMD_FRAME_LENGTH) != ESP_OK)
                         {
+                            ESP_LOGW(TAG, "Frame failed checksum verification, scanning buffer for additional frames...");
                             continue;
                         }
 
+                        ESP_LOGI(TAG, "Frame checksum verification ok!");
                         if(frame_data[ZH03_CMD_RETURN] == dormancy_state_change_fail)
                         {
                             ESP_LOGI(TAG, "Device executed power state change command unsuccessfully");
@@ -291,7 +305,7 @@ static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh0
                         ESP_LOGI(TAG, "Device executed power state change command successfully");
                         return ESP_OK;
                     }
-                    else if(command_type_sent == ZH03_CMD_QA_MODE && frame_data[ZH03_CMD_ID_POS] == measurement_query_response_id)
+                    else if(command_type_sent == ZH03_CMD_RESPONSE && frame_data[ZH03_CMD_ID_POS] == measurement_query_response_id)
                     {
                         if(PM1 == NULL && PM2_5 == NULL && PM10 == NULL)
                         {
@@ -299,9 +313,9 @@ static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh0
                         }
 
                         ESP_LOGD(TAG, "Successfully found response to measurement query");
-                        esp_err_t ret = zh03_verify_checksum(frame_data[ZH03_CMD_ID_POS], ZH03_CMD_FRAME_LENGTH-1);
-                        if(ret != ESP_OK)
+                        if(zh03_verify_checksum(ZH03_QUERIED_MEASUREMENT, frame_data, ZH03_CMD_FRAME_LENGTH) != ESP_OK)
                         {
+                            ESP_LOGW(TAG, "Frame failed checksum verification, scanning buffer for additional frames...");
                             continue;
                         }
 
@@ -310,24 +324,24 @@ static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh0
                         */
                         if(PM_1 != NULL)
                         {
-                            *PM1 = (256*(frame_data[ZH03_QA_PM1_HIGH_BYTE])) + frame_data[ZH03_QA_PM1_LOW_BYTE];
-                            ESP_LOGD(TAG, "PM 1 value is %d ug/m3", (int)*PM_1);
+                            *PM1 = conv_int_8_16(frame_data[ZH03_QA_PM1_HIGH_BYTE], frame_data[ZH03_QA_PM1_LOW_BYTE]);
+                            ESP_LOGI(TAG, "PM 1 value is %d μg/m³", (int)*PM_1);
                         }
                         if(PM_2_5 != NULL)
                         {
-                            *PM2_5 = (256*(frame_data[ZH03_QA_PM2_5_HIGH_BYTE])) + frame_data[ZH03_QA_PM2_5_LOW_BYTE];
-                            ESP_LOGD(TAG, "PM 2.5 value is %d ug/m3", (int)*PM_2_5);
+                            *PM2_5 = conv_int_8_16(frame_data[ZH03_QA_PM2_5_HIGH_BYTE], frame_data[ZH03_QA_PM2_5_LOW_BYTE]);
+                            ESP_LOGI(TAG, "PM 2.5 value is %d μg/m³", (int)*PM_2_5);
                         }
                         if(PM_10 != NULL)
                         {
-                            *PM10 = (256*(frame_data[ZH03_QA_PM10_HIGH_BYTE])) + frame_data[ZH03_QA_PM10_LOW_BYTE];
-                            ESP_LOGD(TAG, "PM 10 value is %d ug/m3", (int)*PM_10);
+                            *PM10 =conv_int_8_16(frame_data[ZH03_QA_PM10_HIGH_BYTE], frame_data[ZH03_QA_PM10_LOW_BYTE]);
+                            ESP_LOGI(TAG, "PM 10 value is %d μg/m³", (int)*PM_10);
                         }
                         return ESP_OK;
                     }
                     else
                     {
-                        ESP_LOGD(TAG, "Found other response frame type...continuing buffer scan!");
+                        ESP_LOGD(TAG, "Found unknown frame type, scanning buffer for additional frames...");
                         continue;
                     }
                 }
@@ -342,7 +356,7 @@ static esp_err_t zh03_get_cmd_response(const uint8_t* in_data, int rx_bytes, zh0
             
         }
         ESP_LOGE(TAG, "Valid response frame not found in buffer!");
-        return ESP_FAIL;
+        return ESP_ERR_NOT_FOUND;
     }
     ESP_LOGE(TAG, "Bytes in UART buffer are less than the expected "#ZH03_CMD_FRAME_LENGTH" byte frame length, try increasing the wait time");
     return ESP_ERR_INVALID_SIZE;
@@ -372,16 +386,14 @@ esp_err_t zh03_sleep(const ZH03* zh03_inst)
     uint8_t data[RX_BUF_SIZE]={0};
     int rx_bytes = 0;
 
-    esp_err_t ret = zh03_uart_write(zh03_inst, set_dormant_mode_cmd_frame, ZH03_CMD_FRAME_LENGTH);
-    if(ret != ESP_OK)
+    if(zh03_uart_write(zh03_inst, set_dormant_mode_cmd_frame, ZH03_CMD_FRAME_LENGTH) != ESP_OK)
     {
         ESP_LOGE(TAG, "Sending command unsuccessful");
         return ret;
     }
 
     ESP_LOGI(TAG, "Successfully sent command");
-    ret = zh03_uart_read(zh03_inst, data, &rx_bytes)
-    if(ret != ESP_OK)
+    if(zh03_uart_read(zh03_inst, data, &rx_bytes) != ESP_OK)
     {
         ESP_LOGE(TAG, "Did not receive data from device");
         return ret;
@@ -401,17 +413,15 @@ esp_err_t zh03_wake(const ZH03* zh03_inst)
 
     uint8_t data[RX_BUF_SIZE]={0};
     int rx_bytes = 0;
-
-    esp_err_t ret = zh03_uart_write(zh03_inst, unset_dormant_mode_cmd_frame, ZH03_CMD_FRAME_LENGTH);
-    if(ret != ESP_OK)
+ 
+    if(zh03_uart_write(zh03_inst, unset_dormant_mode_cmd_frame, ZH03_CMD_FRAME_LENGTH) != ESP_OK)
     {
         ESP_LOGE(TAG, "Sending command unsuccessful");
         return ret;
     }
 
     ESP_LOGI(TAG, "Successfully sent command");
-    ret = zh03_uart_read(zh03_inst, data, &rx_bytes);
-    if(ret != ESP_OK)
+    if(zh03_uart_read(zh03_inst, data, &rx_bytes) != ESP_OK)
     {
         ESP_LOGE(TAG, "Did not receive data from device");
         return ret;
@@ -451,16 +461,14 @@ esp_err_t get_QA_measurement(const ZH03* zh03_inst, uint16_t* PM1, uint16_t* PM2
     uint8_t data[RX_BUF_SIZE]={0};
     int rx_bytes = 0;
 
-    esp_err_t ret = zh03_uart_write(zh03_inst, measurement_query_frame, ZH03_CMD_FRAME_LENGTH);
-    if(ret != ESP_OK)
+    if(zh03_uart_write(zh03_inst, measurement_query_frame, ZH03_CMD_FRAME_LENGTH) != ESP_OK)
     {
         ESP_LOGE(TAG, "Sending command unsuccessful");
         return ret;
     }
 
     ESP_LOGI(TAG, "Successfully sent command");
-    ret = zh03_uart_read(zh03_inst, data, &rx_bytes);
-    if(ret != ESP_OK)
+    if(zh03_uart_read(zh03_inst, data, &rx_bytes) != ESP_OK)
     {
         ESP_LOGE(TAG, "Did not receive data from device");   
         return ret;  
@@ -497,64 +505,62 @@ esp_err_t get_buffered_initiative_upload_measurement(const ZH03* zh03_inst, uint
         return ESP_ERR_INVALID_ARG;
     }
     
-    uint8_t data[RX_BUF_SIZE]={0};
+    uint8_t* frame_data = NULL;
+    uint8_t in_data[RX_BUF_SIZE]={0};
     int rx_bytes = 0;
 
-    esp_err_t ret = zh03_uart_read(zh03_inst, data, &rx_bytes);
-    if(ret != ESP_OK)
+    if(zh03_uart_read(zh03_inst, in_data, &rx_bytes) != ESP_OK)
     {
-        ESP_LOGE(TAG, "Initiative measurement not received", command);
+        ESP_LOGE(TAG, "Did not receive data from device");
         return ret;   
     }
 
-    ESP_LOGI(TAG, "Initiative measurement received", command_name);
+    ESP_LOGI(TAG, "Received data from device");
     if (rx_bytes >= ZH03_INITIATIVE_MEASUREMENT_FRAME_LENGTH) 
     {
-        for(int i=0; i < rx_bytes-1; i++)
+        for(int i=0; i < rx_bytes; i++)
         {
-            if(data[i] == start_of_response_frame_byte_1)
+            if(i <= (rx_bytes - ZH03_INITIATIVE_MEASUREMENT_FRAME_LENGTH))
             {
-                ESP_LOGD(TAG, "Found first start of frame byte");
-                if(data[i+ZH03_START_BYTE_2_POS] == start_of_response_frame_byte_2)
+                if(data[i] == start_of_response_frame_byte_1)
                 {
-                    ESP_LOGD(TAG, "Found second start of frame byte");
-                    esp_err_t ret = zh03_verify_checksum_16(data[i+ZH03_FRAME_LENGTH_POS], ZH03_CMD_FRAME_LENGTH-2);
-                    for(int j = 0; j < () + 2; j++)
+                    ESP_LOGD(TAG, "Found first start of frame byte");
+                    
+                    frame_data = &(in_data[i]);
+                    if(frame_data[ZH03_UPLOAD_START_BYTE_2_POS] == start_of_response_frame_byte_2)
                     {
-                        checksum += data[i+j];
-                    }
-                    checksum = (checksum)%0xffff;
-                    ESP_LOGD(TAG, "Calculated checksum is 0x%04x", (int)checksum);
-                    sent_checksum = data[i+(ZH03_CHECKSUM_LOW_BYTE_POS-1)] + (256*data[i+(ZH03_CHECKSUM_HIGH_BYTE_POS-1)]);
-                    ESP_LOGD(TAG, "Sent checksum is 0x%04x", (int)sent_checksum);
-                    if(checksum != sent_checksum)
-                    {
-                        ESP_LOGE(TAG, "Checksum failed!");
-                        return ESP_ERR_INVALID_CRC;
-                    }
-                    else
-                    {                    
+                        ESP_LOGD(TAG, "Found second start of frame byte");
+
+                        if(zh03_verify_checksum(ZH03_INITIATIVE_MEASUREMENT, frame_data, ZH03_INITIATIVE_MEASUREMENT_FRAME_LENGTH) != ESP_OK)
+                        {
+                            ESP_LOGE(TAG, "Frame failed checksum verification, scanning buffer for additional frames...");
+                            continue;
+                        }
+
+                        ESP_LOGI(TAG, "Frame checksum verification ok!");                 
                         if(PM_1 != NULL)
                         {
-                            *PM_1 = data[i+(ZH03_PM_1_LOW_BYTE_POS-1)] + (256*data[i+(ZH03_PM_1_HIGH_BYTE_POS-1)]);
-                            ESP_LOGD(TAG, "PM 1 value is %d ug/m3", (int)*PM_1);
+                            *PM_1 = conv_int_8_16(data[ZH03_UPLOAD_PM1_HIGH_BYTE_POS], data[ZH03_UPLOAD_PM1_LOW_BYTE_POS]);
+                            ESP_LOGD(TAG, "PM 1 value is %d μg/m³", (int)*PM_1);
                         }
                         if(PM_2_5 != NULL)
                         {
-                            *PM_2_5 = data[i+(ZH03_PM_2_5_LOW_BYTE_POS-1)] + (256*data[i+(ZH03_PM_2_5_HIGH_BYTE_POS-1)]);
-                            ESP_LOGD(TAG, "PM 2.5 value is %d ug/m3", (int)*PM_2_5);
+                            *PM_2_5 = conv_int_8_16(data[ZH03_UPLOAD_PM2_5_HIGH_BYTE_POS], data[ZH03_UPLOAD_PM2_5_LOW_BYTE_POS]);
+                            ESP_LOGD(TAG, "PM 2.5 value is %d μg/m³", (int)*PM_2_5);
                         }
                         if(PM_10 != NULL)
                         {
-                            *PM_10 = data[i+(ZH03_PM_10_LOW_BYTE_POS-1)] + (256*data[i+(ZH03_PM_10_HIGH_BYTE_POS-1)]);
-                            ESP_LOGD(TAG, "PM 10 value is %d ug/m3", (int)*PM_10);
+                            *PM_10 = conv_int_8_16(data[ZH03_UPLOAD_PM10_HIGH_BYTE_POS], data[ZH03_UPLOAD_PM10_LOW_BYTE_POS]);;
+                            ESP_LOGD(TAG, "PM 10 value is %d μg/m³", (int)*PM_10);
                         }
                         return ESP_OK;
                     }
-                }
 
+                }
             }
         }
+        ESP_LOGE(TAG, "Valid response frame not found in buffer!");
+        return ESP_ERR_NOT_FOUND;
     }
     ESP_LOGE(TAG, "Bytes read are less than frame length");
     return ESP_ERR_INVALID_SIZE;
