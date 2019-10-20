124676cb999e65be3edff2fc31908222f718ef31
Alois Mbutura <aloismbutura@gmail.com>
2019-10-20 22:31:13 +0300

    Removed spacings in uart_zh03.h header file

--- IAQ_prototype/Hardware/firmware/components/drivers_particulate_matter_sensors/include/uart_zh03.h
+++ IAQ_prototype/Hardware/firmware/components/drivers_particulate_matter_sensors/include/uart_zh03.h
@@ -10,17 +10,11 @@ typedef struct
 }ZH03;
 
 esp_err_t zh03_init(ZH03* zh03_inst, uart_port_t uart_num, int txd_pin, int rxd_pin);
-
 esp_err_t zh03_sleep(const ZH03* zh03_inst);
-
 esp_err_t zh03_wake(const ZH03* zh03_inst);
-
 esp_err_t set_QA_mode(const ZH03* zh03_inst);
-
 esp_err_t get_QA_measurement(const ZH03* zh03_inst, uint16_t* PM1, uint16_t* PM2_5, uint16_t* PM10);
-
 esp_err_t set_initiative_upload_mode(const ZH03* zh03_inst);
-
 esp_err_t get_buffered_initiative_upload_measurement(const ZH03* zh03_inst, uint16_t* PM1, uint16_t* PM2_5, uint16_t* PM10);
 
 #endif
